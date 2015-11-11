91cd25c18d356c6134acd38fd1b567bae595b4f7
sabeekpradhan <sabeekpradhan@gmail.com>
2015-11-10 16:38:16 -0800

    Reformatted to remove iPython notebook holdovers

--- csv_parser.py
+++ csv_parser.py
@@ -1,17 +1,9 @@
-
-# coding: utf-8
-
-# In[1]:
-
 #!/usr/bin/python
 
 
 import sys, csv, re, os, time
 import sqlite3 as sql
 from itertools import chain
-
-
-# In[31]:
 
 db_name = 'master.db'
 csv_1982 = 'Data/contribDB_1982.csv'
@@ -20,9 +12,6 @@ contributors_path = 'Data/contributor_cfscores_st_fed_1979_2012.csv'
 loadAllTransactionFilesInDir(db_name, 'Data/')
 loadRecipients(db_name, recipient_path)
 loadContributors(db_name, contributors_path)
-
-
-# In[2]:
 
 def loadAllTransactionFilesInDir(dbName, dirpath):
     print '------------- Loading All Transaction Tables -------------'
@@ -33,16 +22,10 @@ def loadAllTransactionFilesInDir(dbName, dirpath):
         loadTransactionFile(dbName, dirpath + tf, int(tf.split('_')[1][0:4]))
     print 'Total time taken: ' + str(time.time() - start) 
 
-
-# In[3]:
-
 def listdir_nohidden(path):
     for f in os.listdir(path):
         if not f.startswith('.'):
             yield f
-
-
-# In[4]:
 
 def loadTransactionFile(dbName, filepath, year):
     print 'Loading Transactions_{0} into Table:'.format(year)
@@ -56,9 +39,6 @@ def loadTransactionFile(dbName, filepath, year):
         commitTransBlock(dbName, block)
         
     print 'Time taken: ' + str(time.time() - start)
-
-
-# In[5]:
 
 def loadRecipients(dbName, filepath):
     print '------------- Loading Recipients Table -------------'
@@ -76,9 +56,6 @@ def loadRecipients(dbName, filepath):
     
     print 'Time taken: ' + str(time.time() - start) 
 
-
-# In[6]:
-
 def loadContributors(dbName, filepath):
     print '------------- Loading Contributors Table -------------'
     start = time.time()
@@ -93,9 +70,6 @@ def loadContributors(dbName, filepath):
     
     print 'Time taken: ' + str(time.time() - start) 
 
-
-# In[7]:
-
 # Ensures that all recipients have unique (year, rid, seat) keys
 # and that only the first row is taken.
 def extractDuplicateRecipients(block, observedKeys):
@@ -106,13 +80,7 @@ def extractDuplicateRecipients(block, observedKeys):
             observedKeys.add((l[0], l[1], l[3]))
     return newBlock
 
-
-# In[8]:
-
 # ----- Column Transformation Functions -----
-
-
-# In[9]:
 
 def getTransID(code):
     fullID = code.split(':')
@@ -121,53 +89,32 @@ def getTransID(code):
     else:
         return int(fullID[2])
 
-
-# In[10]:
-
 def strToFltToInt(num):
     if (num == ''): return None
     return int(float(num))
 
-
-# In[11]:
-
 def safeFloat(num):
     if (num == ''): return None
     return float(num)
-
-
-# In[12]:
 
 def getRecipientInfo(code):
     if (code == ""): return 0 # Return 0 if no recipient ID available
     if (code[0:4] == "cand"): return int(re.findall(r'\d+', code)[0])
     else: return -int(re.findall(r'\d+', code)[0]) # Negative recipient ID if not 'candidate'
 
-
-# In[13]:
-
 def indiv(code):
     if (code == 'I'): return 1
     elif (code == 'C'): return 0
     else: return None
 
-
-# In[14]:
-
 def safeInt(num):
     if (num == ''): return None
     return int(num)
-
-
-# In[15]:
 
 def winner(code):
     if (code == 'L'): return 0
     elif (code == 'W'): return 1
     else: return None
-
-
-# In[16]:
 
 def party(code):
     try:
@@ -181,25 +128,16 @@ def party(code):
     except:
         return 3
 
-
-# In[17]:
-
 def incumb(code):
     if (code == 'O'): return 0
     elif (code == 'I'): return 1
     elif (code == 'C'): return 2
     else: return None
 
-
-# In[18]:
-
 def gender(code):
     if (code == 'F'): return 1
     elif (code == 'M'): return 0
     else: return None # '' or 'U'
-
-
-# In[19]:
 
 def candStatus(code):
     if (code == 'C'): return 1
@@ -208,21 +146,12 @@ def candStatus(code):
     elif (code == 'P'): return 4
     else: return None
 
-
-# In[20]:
-
 def candOrComm(code):
     if (code == 'comm'): return 0
     elif (code == 'cand'): return 1
     else: return None
 
-
-# In[21]:
-
 # ----- Block Commit Functions -----
-
-
-# In[22]:
 
 def commitContribBlock(dbName, block):
     
@@ -245,9 +174,6 @@ def commitContribBlock(dbName, block):
 
         if con: con.close() 
 
-
-# In[23]:
-
 def commitTransBlock(dbName, block):
 
     con = None
@@ -267,9 +193,6 @@ def commitTransBlock(dbName, block):
     finally:
 
         if con: con.close() 
-
-
-# In[24]:
 
 def commitRecipBlock(dbName, block):
 
@@ -292,9 +215,6 @@ def commitRecipBlock(dbName, block):
 
         if con: con.close() 
 
-
-# In[25]:
-
 # Takes a CSV reader, the column indexes we are interested in, and the
 # function transformations for each of those indexes, and returns a
 # list of tuples, corresponding to the list of processed rows for our sql table.
@@ -314,13 +234,7 @@ def generateChunk(reader, extractors, transforms, chunksize=20000):
         chunk.append(tuple(chain(*(i if isinstance(i, tuple) else (i,) for i in processedLine))))
     yield chunk
 
-
-# In[26]:
-
 # ----- Table Init Functions -----
-
-
-# In[27]:
 
 # Initializes a Transaction table for a particular year:
 # Columns: [1, 2, 3, 4, 5, 27, 34]
@@ -357,9 +271,6 @@ def initTransactionsTable(dbName):
 
         if con: con.close() 
 
-
-# In[28]:
-
 # Initializes the Contributors table:
 # Columns: [0, 1, 2]
 def initContributorsTable(dbName):
@@ -388,9 +299,6 @@ def initContributorsTable(dbName):
     finally:
 
         if con: con.close() 
-
-
-# In[29]:
 
 # Columns: [0, 7, 10, 12, 13, 14, 15, 16, 22, 23, 39, 46, 47, 61, 62, 63, 64, 65]
 # 39-ran primary  , 46 winner, 47 district partisanship, 61 in district donations
@@ -434,9 +342,6 @@ def initRecipientTable(dbName):
     finally:
         if con: con.close()
 
-
-# In[30]:
-
 # ----- USEFUL CODE FOR DEBUGGING: -----
 
 # lst = []
@@ -451,9 +356,3 @@ def initRecipientTable(dbName):
 # for i, f in enumerate(transforms):
 #     print i, f
 #     f(xst[i])
-
-
-# In[ ]:
-
-
-
