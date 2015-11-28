c44bcbbcba4116dc6a075bb21cd6a8c07735e1f3
Sabeek Pradhan <sabeekpradhan@gmail.com>
2015-11-28 06:25:57 +0000

    Added functions to save graphs and iterate over edges adjacent to a node or nodeset

--- src/util/graph_funcs.py
+++ src/util/graph_funcs.py
@@ -9,6 +9,12 @@ import snap
 def loadGraph(filename, graphType = snap.TNEANet):
     return graphType.Load(snap.TFIn(filename))
 
+# Save a snap graph to a file
+def saveGraph(graph, filename):
+    FOut = snap.TFOut(filename)
+    graph.save(FOut)
+    FOut.Flush()
+
 # Generator over all the recipient nodes in the graph
 def getRecipients(graph):
     for node in graph.Nodes():
@@ -20,3 +26,18 @@ def getDonors(graph):
     for node in graph.Nodes():
         if graph.GetIntAttrDatN(node.GetId(), 'IsRecip') == 0:
             yield node
+
+# Generator over all the edges connected to this node id
+def getEdgesWithNode(graph, nodeid):
+    for edge in graph.Edges():
+        if edge.GetSrcNId() == nodeid or edge.GetDstNId() == nodeid:
+            yield edge
+
+# Generator over all the edges connected to one of the node ids in this
+# collection
+def getEdgesWithNodeset(graph, nodeids):
+    idSet = set(nodeids)
+    for edge in graph.Edges():
+        if edge.GetSrcNId() in idSet or edge.GetDstNId() in idSet:
+            yield edge
+
